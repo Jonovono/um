463d5f3781f4c54e99e926a8d1a4f7f016a9cfef
Jordan Howlett <jordan@howlett.io>
2013-11-24 13:41:11 -0600

    Add a Gruntfile for linting (and soon testing)

---
+++ Gruntfile.js
@@ -0,0 +1,43 @@
+/*global module:false*/
+
+module.exports = function (grunt) {
+
+  "use strict";
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-simple-mocha');
+
+
+  // Project configuration.
+  grunt.initConfig({
+
+    jshint: {
+      files: [
+        'Gruntfile.js',
+        'lib/**/*.js',
+        'bin/*.js'
+      ]
+    },
+
+    simplemocha: {
+      options: {
+        reporter: 'spec',
+        node_env: 'testing',
+        ignoreLeaks: true
+      },
+      full: { src: ['spec/runner.js'] }
+    },
+
+    watch: {
+      files: ['<%= jshint.files %>'],
+      tasks: 'jshint'
+    }
+
+  });
+
+  // Default task.
+  grunt.registerTask('default', ['jshint', 'simplemocha:full']);
+  grunt.registerTask('test', ['jshint', 'simplemocha:full']);
+
+};
